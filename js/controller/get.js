f5aab217f54152fc542f64109736bf85df42b936
Fatwa <fax.mp4@gmail.com>
2023-04-01 12:32:05 +0700

    update

--- js/controller/get.js
+++ js/controller/get.js
@@ -8,15 +8,15 @@ function isiRow(value) {
   let content = isiTabel
     .replace("#NAMA#", value.biodata.nama)
     .replace("#NPM#", value.biodata.npm)
-    .replace("#KELAS#", value.biodata.namakelas.namakelas)
-    .replace("#JURUSAN#", value.biodata.jurusan.namaprodi)
+    .replace("#KELAS#", value.biodata.nama_kelas.nama_kelas)
+    .replace("#JURUSAN#", value.biodata.jurusan.nama_prodi)
     .replace("#STATUS#", value.kehadiran == "masuk" ? value.kehadiran : "menghilang")
-    .replace("#MATKUL#", value.matakuliah.namamatkul)
-    .replace("#DOSEN#", value.matakuliah.dosenpengajar.namadosen)
-    .replace("#HARIKULIAH#", value.matakuliah.jadwalkuliah.hari ? value.matakuliah.jadwalkuliah.hari : "#HARIKULIAH")
-    .replace("#JAMMASUK#", value.matakuliah.jadwalkuliah.hari ? value.matakuliah.jadwalkuliah.jammasuk : "#JAMMASUK")
-    .replace("#JAMKELUAR#", value.matakuliah.jadwalkuliah.hari ? value.matakuliah.jadwalkuliah.jamkeluar : "#JAMMKELUAR")
-    .replace("#RUANGAN#", value.matakuliah.ruangkuliah.namaruang ? value.matakuliah.ruangkuliah.namaruang : "#RUANGAN")
+    .replace("#MATKUL#", value.mata_kuliah.nama_matkul)
+    .replace("#DOSEN#", value.mata_kuliah.dosen_pengajar.nama_dosen)
+    .replace("#HARIKULIAH#", value.mata_kuliah.jadwal_kuliah.hari ? value.mata_kuliah.jadwal_kuliah.hari : "#HARIKULIAH")
+    .replace("#JAMMASUK#", value.mata_kuliah.jadwalkuliah.hari ? value.mata_kuliah.jadwal_kuliah.jam_masuk : "#JAMMASUK")
+    .replace("#JAMKELUAR#", value.mata_kuliah.jadwalkuliah.hari ? value.mata_kuliah.jadwal_kuliah.jam_keluar : "#JAMMKELUAR")
+    .replace("#RUANGAN#", value.mata_kuliah.ruang_kuliah.nama_ruang ? value.mata_kuliah.ruang_kuliah.nama_ruang : "#RUANGAN")
     .replace("#WARNA#", getRandomColor())
     .replace(/#WARNALOGO#/g, getRandomColorName());
   addInner("iniTabel", content);
