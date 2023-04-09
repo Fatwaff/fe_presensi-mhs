c699199a2878e7973f1d3f2eb4a4c5fe7ce00ce7
Fatwa <fax.mp4@gmail.com>
2023-04-09 21:00:29 +0700

    update banyak

---
+++ js/activeTable.js
@@ -0,0 +1,63 @@
+function showPresensi() {
+  document.getElementById("iniTabelPresensi").style.display = "";
+  document.getElementById("iniTabelMahasiswa").style.display = "none";
+  document.getElementById("iniTabelKelas").style.display = "none";
+  document.getElementById("iniTabelProdi").style.display = "none";
+  document.getElementById("iniTabelMatkul").style.display = "none";
+  document.getElementById("iniTabelDosen").style.display = "none";
+  document.getElementById("iniTabelRuangan").style.display = "none";
+}
+function showMahasiswa() {
+  document.getElementById("iniTabelPresensi").style.display = "none";
+  document.getElementById("iniTabelMahasiswa").style.display = "";
+  document.getElementById("iniTabelKelas").style.display = "none";
+  document.getElementById("iniTabelProdi").style.display = "none";
+  document.getElementById("iniTabelMatkul").style.display = "none";
+  document.getElementById("iniTabelDosen").style.display = "none";
+  document.getElementById("iniTabelRuangan").style.display = "none";
+}
+function showKelas() {
+  document.getElementById("iniTabelPresensi").style.display = "none";
+  document.getElementById("iniTabelMahasiswa").style.display = "none";
+  document.getElementById("iniTabelKelas").style.display = "";
+  document.getElementById("iniTabelProdi").style.display = "none";
+  document.getElementById("iniTabelMatkul").style.display = "none";
+  document.getElementById("iniTabelDosen").style.display = "none";
+  document.getElementById("iniTabelRuangan").style.display = "none";
+}
+function showProdi() {
+  document.getElementById("iniTabelPresensi").style.display = "none";
+  document.getElementById("iniTabelMahasiswa").style.display = "none";
+  document.getElementById("iniTabelKelas").style.display = "none";
+  document.getElementById("iniTabelProdi").style.display = "";
+  document.getElementById("iniTabelMatkul").style.display = "none";
+  document.getElementById("iniTabelDosen").style.display = "none";
+  document.getElementById("iniTabelRuangan").style.display = "none";
+}
+function showMatkul() {
+  document.getElementById("iniTabelPresensi").style.display = "none";
+  document.getElementById("iniTabelMahasiswa").style.display = "none";
+  document.getElementById("iniTabelKelas").style.display = "none";
+  document.getElementById("iniTabelProdi").style.display = "none";
+  document.getElementById("iniTabelMatkul").style.display = "";
+  document.getElementById("iniTabelDosen").style.display = "none";
+  document.getElementById("iniTabelRuangan").style.display = "none";
+}
+function showDosen() {
+  document.getElementById("iniTabelPresensi").style.display = "none";
+  document.getElementById("iniTabelMahasiswa").style.display = "none";
+  document.getElementById("iniTabelKelas").style.display = "none";
+  document.getElementById("iniTabelProdi").style.display = "none";
+  document.getElementById("iniTabelMatkul").style.display = "none";
+  document.getElementById("iniTabelDosen").style.display = "";
+  document.getElementById("iniTabelRuangan").style.display = "none";
+}
+function showRuangan() {
+  document.getElementById("iniTabelPresensi").style.display = "none";
+  document.getElementById("iniTabelMahasiswa").style.display = "none";
+  document.getElementById("iniTabelKelas").style.display = "none";
+  document.getElementById("iniTabelProdi").style.display = "none";
+  document.getElementById("iniTabelMatkul").style.display = "none";
+  document.getElementById("iniTabelDosen").style.display = "none";
+  document.getElementById("iniTabelRuangan").style.display = "";
+}
